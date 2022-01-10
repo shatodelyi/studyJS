e1d48912e04cf97845231317d53e87f33ac776ec
Vasiliy <vasiliyrodionov@gmail.com>
2022-01-10 20:08:42 +0200

    first

---
+++ js/1.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let number = 5;
+console.log(number);
+
+const leftBorderWidth = 1;
+number += leftBorderWidth;
+console.log(number);
+
+
+let number1 = 4.6;
+
+console.log(number1 / 0);
+console.log('string' * 9);
+
+const person = 'Alex';
+console.log(person);
+
+const bool = true || false;
+
+let und;
+console.log(und);
+
+//console.log(something);
+
+const obj = {
+    name: 'John',
+    age: 24,
+    isMarried: false
+};
+
+console.log(obj.age + ' ' + obj.name);
+// console.log(obj['name']);
+
+let arr = ['plum.png', 'orange.jpg', 5, false];
+console.log(arr[2]);
+arr.push(213);
+console.log(arr);
---
+++ js/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// alert('hello!');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Whats your name?', '');
+// answers[1] = prompt('Whats your lastname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof(null));
+
+
+//Интерполяция
+
+// const category = 'toys';
+// console.log(`http://someurl.com/toys${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+//Операторы
+
+// console.log('arr' + ' - object');
+// console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+console.log(isChecked && !isClose);
+
