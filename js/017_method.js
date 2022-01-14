0ae4a350138add463476259de03ede3f560525fe
Vasiliy <vasiliyrodionov@gmail.com>
2022-01-14 18:39:39 +0200

    017_method

---
+++ js/017_method.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const str = 'test';
+const arr = [1, 2, 4];
+
+console.log(arr.length);
+console.log(str[2]);
+/*
+
+console.log(str.toUpperCase()); //toLowerCase
+
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('fruit'));
+console.log(fruit.indexOf('q'));
+
+const logg = "Hello world";
+
+console.log(logg.slice(3, 11));
+console.log(logg.slice(6));
+console.log(logg.slice(-5, -1));
+
+console.log(logg.substring(6, 11)); // не поддерживает отрицательные
+
+console.log(logg.substr(6, 2));
+*/
+
+const num = 12.1;
+console.log(Math.round(num));
+
+const test = '15.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
