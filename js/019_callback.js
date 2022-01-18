bac88bfa88959b7e9470ee0d49a31a3c78d9250e
Vasiliy <vasiliyrodionov@gmail.com>
2022-01-18 19:08:41 +0200

    019

---
+++ js/019_callback.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/*
+function first() {
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+*/
+
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('I complete this lesson!');
+}
+
+learnJS('JavaScript', done);
